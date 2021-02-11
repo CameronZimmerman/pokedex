d5f48fae4ceeb68b449ac9358f6c4b25d6143504
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-11 14:45:36 -0800

    create filtering based off of state

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -5,24 +5,40 @@ export default class Pokedex extends Component {
 
     state = {
         pokemon: pokedata,
-        sortBy: '',
+        sortBy: 'id',
         filter: '',
         sortOrder: 'forwards'
     }
-
-    handleFilter = () => {
-        
+    // this will filter by pokemon name in searchBar
+    handleFilter = (e) => {
+        this.setState({
+            filter: e.target.value
+        })
     }
-
-    handleSortBy = () => {
-
+    // this will set sortby to the users category choice in a dropdown
+    handleSortBy = (e) => {
+        this.setState({
+            sortBy: e.target.value
+        })
     }
-
-    handleSortOrder = () => {
+    // this will invert search order
+    handleSortOrder = (e) => {
 
     }
     
     render() {
+        const sortByType = typeof this.state.pokemon[0][this.state.sortBy];
+        console.log(sortByType);
+        //sort pokemon based off of state.sortBy
+        if(this.state.sortOrder === 'forwards') {
+            if (sortByType === 'string') this.state.pokemon.sort((a, b) => a[this.state.sortBy].localeCompare(b[this.state.sortBy]));
+            if(sortByType === 'number') this.state.pokemon.sort((a, b) => a[this.state.sortBy] - b[this.state.sortBy]);
+        }
+        else {
+            if (sortByType === 'string') this.state.pokemon.sort((a, b) => b[this.state.sortBy].localeCompare(a[this.state.sortBy]));
+            if(sortByType === 'number') this.state.pokemon.sort((a, b) => b[this.state.sortBy] - a[this.state.sortBy]);
+        }
+        //set filtered array based off of sorted state and pass to pokelist as props
         return (
             <p>Pokedex</p>
         )
